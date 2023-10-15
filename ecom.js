01efad369ed0fa4d76a2bbe99bdb4807cc7a0e1f
ishu7219 <147970206+ishu7219@users.noreply.github.com>
2023-10-15 00:44:39 -0700

    Add files via upload

---
+++ ecom.js
@@ -0,0 +1,15 @@
+let login = document.querySelector('.login');
+
+document.querySelector('#login').onclick = () => {
+    login.classList.toggle('active');
+}
+let search = document.querySelector('.search');
+
+document.querySelector('#search').onclick = () => {
+    search.classList.toggle('active');
+}
+let favorite = document.querySelector('.favorite');
+
+document.querySelector('#favorite').onclick = () => {
+    favorite.classList.toggle('active');
+}
